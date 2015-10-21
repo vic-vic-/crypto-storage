5babcafe35f4d42143bd0a611fc7715e573e53d9
Victor Veliz <vicsnatch2@hotmail.com>
2015-10-20 22:19:34 -0700

    Added python module for database.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,35 @@
-"""
-Definition of models.
+﻿"""
+Definition of models. This is our main model for creating and 
+defining our information in the database.
 """
 
 from django.db import models
 
-# Create your models here.
+
+class RegisterUser(models.Model):
+    """ Register User for CryptoStorage """
+    email       = models.EmailField(max_length=200, primary_key=True)
+    first_name  = models.CharField(max_length=60, null=False, blank=False)
+    last_name   = models.CharField(max_length=60, null=False, blank=False)
+    date_joined = models.DateTimeField(auto_now_add=True, auto_now=False)
+    date_update = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        """ To display the email on the template """
+        return self.email
+
+class File(models.Model):
+    """ The File table to contain all information of user's files """
+    file_name  = models.CharField(max_length=200, null=False, blank=False)
+    file_hash  = models.CharField(max_length=32, null=False, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True, auto_now=False)
+    user = models.ForeignKey(RegisterUser)
+
+    def __unicode__(self):
+        return self.file_name
+
+class PassUser(models.Model):
+    """ Password Table of users """
+    pass_hash = models.CharField(max_length=200,null=False, blank=False)
+    pass_salt = models.CharField(max_length=200,null=False, blank=False)
+    user = models.OneToOneField(RegisterUser)
